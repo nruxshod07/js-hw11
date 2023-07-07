1d9f0730795c7c7153bd0a95e7a55de669e13380
nruxshod07 <nezametdinovruxsod@gmail.com>
2023-07-07 14:27:12 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let password = "5555"
+let attemps = 5
+let i = 1
+let input
+do {
+    input = prompt("Enter your password")
+    if (input === null) {
+        break;
+    }
+    if (input === password) {
+        alert("Здравствуйте!")
+        break;
+    } else if (i < attemps) {
+        alert(`Неверный пароль, у вас ${attemps - i} попыток!`)
+    } else {
+        alert("Попытки закончились, доступ заблокирован!")
+    }
+    i += 1
+}
+while (attemps >= i) {
+}
+let f = 1
+
+function delay() {
+    setTimeout(function () {
+        f++;
+        if (f < 100) {
+            delay();
+        }
+    }, 1000)
+    console.log("hello");
+    console.log("hello");
+}
+delay()
